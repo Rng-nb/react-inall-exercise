a98d098a65d394f60d552bbe7b5ca984b2faca7d
Rng-nb <18782824515@163.com>
2020-08-13 08:56:11 +0800

    [yafeng Wang] calculate

--- src/app/component/Caculate/Calculate.js
+++ src/app/component/Caculate/Calculate.js
@@ -20,29 +20,29 @@ class Caculate extends React.Component {
           <input className="shownumber" value={this.state.number} /> 
         </div>
           <div>
-            <button className="key yellow" onClick={() => this.getNum('+')}>+</button>
-            <button className="key yellow" onClick={() => this.getNum('-')}>-</button>
-            <button className="key yellow" onClick={() => this.getNum('x')}>x</button>
+            <button  onClick={() => this.getNum('+')}>+</button>
+            <button  onClick={() => this.getNum('-')}>-</button>
+            <button  onClick={() => this.getNum('x')}>x</button>
           </div>
           <div>
-            <button className="key" onClick={() => this.getNum('7')}>7</button>
-            <button className="key" onClick={() => this.getNum('8')}>8</button>
-            <button className="key" onClick={() => this.getNum('8')}>9</button>
+            <button  onClick={() => this.getNum('7')}>7</button>
+            <button  onClick={() => this.getNum('8')}>8</button>
+            <button  onClick={() => this.getNum('8')}>9</button>
           </div>
           <div>
-            <button className="key" onClick={() => this.getNum('4')}>4</button>
-            <button className="key" onClick={() => this.getNum('5')}>5</button>
-            <button className="key" onClick={() => this.getNum('6')}>6</button>
+            <button  onClick={() => this.getNum('4')}>4</button>
+            <button  onClick={() => this.getNum('5')}>5</button>
+            <button  onClick={() => this.getNum('6')}>6</button>
           </div>
           <div>
-            <button className="key" onClick={() => this.getNum('1')}>1</button>
-            <button className="key" onClick={() => this.getNum('2')}>2</button>
-            <button className="key" onClick={() => this.getNum('3')}>3</button>
+            <button  onClick={() => this.getNum('1')}>1</button>
+            <button  onClick={() => this.getNum('2')}>2</button>
+            <button  onClick={() => this.getNum('3')}>3</button>
           </div>
           <div>
-            <button className="key" onClick={() => this.getNum('0')}>0</button>
-            <button className="key clear" onClick={this.clearNum}>clear</button>
-            <button className="key equal" onClick={this.getResult}>=</button>
+            <button  onClick={() => this.getNum('0')}>0</button>
+            <button  onClick={this.clearNum}>clear</button>
+            <button  onClick={this.getResult}>=</button>
           </div>
         </div>
       </div>);
